c2e49ed2aa5cea767a5f91a7a1255e01938dcbf6
Dan Blustein <dansakai0@gmail.com>
2020-08-31 00:25:09 +0900

    Minor tweaks + added requirements.txt file

--- bedbot.py
+++ bedbot.py
@@ -1,10 +1,16 @@
 import discord
 from discord.ext import commands
 
-client = commands.Bot(command_prefix = '.')
+client = commands.Bot(command_prefix = '!')
 
 @client.event
 async def on_ready():
     print("Bot is ready.")
+
+@client.command()
+async def bedbot(ctx):
+    await ctx.send(f'ur stats are bad. \nlatency: {round(client.latency * 1000)}ms')
+    print("Bruh")
+
 
 client.run("token here")
