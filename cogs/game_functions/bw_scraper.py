a21721ad7b90072b87b52d09082651780ee393d5
Dan Blustein <dansakai0@gmail.com>
2020-09-02 14:48:37 +0900

    small changes

--- cogs/game_functions/bw_scraper.py
+++ cogs/game_functions/bw_scraper.py
@@ -3,10 +3,14 @@ import urllib.request
 import time
 from bs4 import BeautifulSoup
 
+SOLOS_INDEX = 0 
+DOUBLES_INDEX = 1
+THREES_INDEX = 2
+FOURS_INDEX = 3
 
 def getBedStats(username: str, gamemode: int):
   if (gamemode == -1):
-    return "Invalid Input: 1's, 2's, 3's, & 4's are the only supported gamemodes."
+    return "Ur trolling, nice try buddy. Invalid Input: 1's, 2's, 3's, & 4's are the only supported gamemodes."
 
   userLink = generateLink(username)
 
@@ -23,14 +27,18 @@ def getBedStats(username: str, gamemode: int):
 
   # Loops through all the tr's that have actual stats in them
   stats = []
+  allstats = []
   for tr in stat_tr:
     if (len(tr.select("td")) == 10): # 10 is the number of stats for each gamemode
       stats.append(tr)
-      for td in tr.select("td"):
-        print(td.text)
+      #for td in tr.select("td"):
+        #print(td.text)
+
+  for td in stats[gamemode - 1]:
+    allstats.append(td.text)
+
         
-        
-  return "Placeholder"  
+  return f'Beds Broken {allstats[10]}'
 
 def generateLink(username: str):
   userLink = 'https://plancke.io/hypixel/player/stats/' + username + '#Bedwars'
