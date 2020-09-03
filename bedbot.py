d411cbcbfa3338768390e49c464aa7c210742c1e
Dan Blustein <dansakai0@gmail.com>
2020-09-03 11:11:52 +0900

    Implement loading/unloading feature of cogs

--- bedbot.py
+++ bedbot.py
@@ -6,6 +6,19 @@ load_dotenv()
 
 client = commands.Bot(command_prefix = '!')
 
+@client.command()
+async def load(ctx, extension):
+    client.load_extension(f'cogs.{extension}')
+
+@client.command()
+async def unload(ctx, extension):
+    client.unload_extension(f'cogs.{extension}')
+
+@client.command()
+async def reload(ctx, extension):
+    client.unload_extension(f'cogs.{extension}')
+    client.load_extension(f'cogs.{extension}')        
+
 cogs = ['cogs.BedWarsCommands', 'cogs.MiscEventsAndCommands']
 
 if __name__ == '__main__':
